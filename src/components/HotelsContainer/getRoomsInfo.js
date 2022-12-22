812ac4df6e770e0a8e4f5a92abf618e035453ec0
NilsonNetto <eng.nilsonnetto@gmail.com>
2022-12-22 16:14:07 -0300

    refactor: roomsInfo refactor

--- src/components/HotelsContainer/getRoomsInfo.js
+++ src/components/HotelsContainer/getRoomsInfo.js
@@ -1,5 +1,5 @@
 
-export default function handleRoomInfo(roomsData) {
+/* export default function handleRoomInfo(roomsData) {
   if (roomsData ===null) return; 
   const roomsCapacity = roomsData.Rooms.map((room) => room.capacity);
   const roomTypes = getRoomTypes(roomsCapacity);
@@ -44,4 +44,27 @@ function getRoomTypes(roomsCapacity) {
   if(roomTypes.includes('triple')) {
     return 'Triple';
   };
+}
+ */
+
+export default function getRoomsInfo(roomsData) {
+  if (roomsData === null) return;
+
+  let roomTypes = Array(3).fill(undefined);
+  const { totalCapacity, bookings } = roomsData.Rooms.reduce(
+    (acc, current) => {
+      const { capacity, Booking } = current;
+
+      if (capacity === 1) roomTypes[0] = 'Single';
+      else if (capacity === 2) roomTypes[1] = 'Double';
+      else if (capacity === 3) roomTypes[2] = 'Triple';
+
+      acc.totalCapacity += capacity;
+      acc.bookings += Booking.length;
+      return acc;
+    },
+    { totalCapacity: 0, bookings: 0 }
+  );
+  roomTypes = roomTypes.filter((type) => type !== undefined);
+  return { roomTypes: new Intl.ListFormat('pt').format(roomTypes), availableBookings: totalCapacity - bookings };
 }
